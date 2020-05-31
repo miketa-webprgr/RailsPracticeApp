317de006a1f06773f82b5619856ab94c82283c3f
miketa_webprgr <ksuedomi91@gmail.com>
2020-05-31 21:37:42 +0900

    メニューバーの作成(application.html.slim) + index.html.slimの作成 + データが表示されるようbill_controllerに追記 #3

--- BuhiManager/app/controllers/bills_controller.rb
+++ BuhiManager/app/controllers/bills_controller.rb
@@ -1,2 +1,25 @@
 class BillsController < ApplicationController
+
+  def index
+    @bills = Bill.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
