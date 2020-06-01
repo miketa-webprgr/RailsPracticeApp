e6f3ed5a14ef9b45bde1e5a48b333ffc6f8f0e86
miketa_webprgr <ksuedomi91@gmail.com>
2020-06-01 18:15:27 +0900

    日本語化（日本語をデフォルトに設定し、ja.ymlを導入）+ 円通貨で表示 #3

--- BuhiManager/config/application.rb
+++ BuhiManager/config/application.rb
@@ -15,5 +15,12 @@ module BuhiManager
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Qiita記事を参考にして、日本語化する
+    # https://qiita.com/shimadama/items/7e5c3d75c9a9f51abdd5
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
   end
 end
