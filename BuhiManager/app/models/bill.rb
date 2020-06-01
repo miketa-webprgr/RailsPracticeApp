5591733d168df482ecf49544f5aba7862a23a0fc
miketa_webprgr <ksuedomi91@gmail.com>
2020-06-02 02:39:28 +0900

    Billモデルのvalidationの修正（カラム名dateがpaid_onになっていたので合わせて修正 + uniquenessが不要だったので削除）#3

--- BuhiManager/app/models/bill.rb
+++ BuhiManager/app/models/bill.rb
@@ -1,7 +1,7 @@
 class Bill < ApplicationRecord
   validates :status, inclusion: {in: [true, false]}
-  validates :name, presence: true, uniqueness: true
-  validates :date, presence: true
+  validates :name, presence: true
+  validates :paid_on, presence: true
   validates :item, presence: true
   validates :price, numericality: { greater_than: 0 }
 end
