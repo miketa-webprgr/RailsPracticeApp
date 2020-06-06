dda5656d981bfd6d00bd826b514bcb06a34e3c36
miketa_webprgr <ksuedomi91@gmail.com>
2020-06-07 08:37:30 +0900

    ログイン・ログアウト機能の実装（機能制限については詳細を参照） #5
    ログインしていないと管理画面にアクセス不可能。ログインしていないと精算済のデータを編集できない。

--- BuhiManager/app/controllers/application_controller.rb
+++ BuhiManager/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  # @current_userにsessionIDが入っている場合、そのidでユーザーモデルからデータを検索し、@current_userに格納するメソッド
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    else
+      @current_user = User.new
+    end
+  end
 end
